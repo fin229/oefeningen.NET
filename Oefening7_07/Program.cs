871cda79c0aff50dd0896d21969c6f0e4ba54c63
Fineas <r0845664@student.thomasmore.be>
2022-11-05 21:44:54 +0100

    dit is de oefening met list.Foreach(console.WriteLine)

---
+++ Oefening7_07/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oefening7_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "appel", "peer", "paard", "ezel", "koffie", "thee"
+                , "water", "keuken", "badkamer", "frigo" };
+
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
